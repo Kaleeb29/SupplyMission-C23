b957f86cb20e31a479054050af1dc4c6581bebc8
Kaleeb29 <69799609+Kaleeb29@users.noreply.github.com>
2020-09-09 00:55:44 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box{
+ constructor(x,y,width,height){
+     var options = {
+
+
+     }
+     this.body = Bodies.rectangle(x,y,width,height,options);
+     this.width = width;
+    this.height = height;
+     World.add(world,this.body);
+ }
+  display(){
+    var pos = this.body.position;
+    rectMode(CENTRE);
+    fill("red");
+    rect(0,0, this.width, this.height);
+  }
+}
